3ee96cf2e61bd1c4c5526ffd09699a2f37cf451f
“chentingkai” <bill800123@gmail.com>
2018-11-10 22:05:52 +0800

    Success

--- Peter_Homework7/ViewController.swift
+++ Peter_Homework7/ViewController.swift
@@ -5,16 +5,41 @@
 //  Created by 陳庭楷 on 2018/11/10.
 //  Copyright © 2018年 陳庭楷. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var count = 0
+    
+    @IBOutlet weak var lightButton: UIButton!
+    
+    @IBAction func light(_ sender: Any) {
+    
+        self.lightButton.backgroundColor = self.lightButton.backgroundColor == .white ? .black : .white
+        
+//        if self.count == 0 {
+//
+//            self.lightButton.backgroundColor = .black
+//
+//            self.count += 1
+//
+//        }
+//
+//        else if self.count == 1 {
+//
+//            self.lightButton.backgroundColor = .white
+//
+//            self.count = 0
+//
+//        }
+        
     }
-
-
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+    }
+    
 }
 
